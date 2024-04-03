714ed30a3cc62c14341b34e0aaaaae176cc060e4
YAZILIM-NB3 <boyrazalpercan@gmail.com>
2024-04-03 16:24:13 +0300

    Add project files.

---
+++ ChatService/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatService.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task JoinRoom(UserConnection userConnection){    
+        
+        }
+}
